7cb008b32d714d35e3f078ab1cfbc6d4699607aa
Zeynep Ozkan <zeynepozkan0320@gmail.com>
2021-10-13 15:26:22 +0200

    Initial commit

---
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -0,0 +1,7 @@
+package com.vytrack.pages;
+
+public abstract class BasePage {
+
+
+
+}
---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -0,0 +1,45 @@
+package com.vytrack.step_definitions;
+
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LoginStepDefs {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+
+        String url= ConfigurationReader.get("url");
+        Driver.get().get(url);
+
+    }
+
+
+    @When("the user enter the driver information")
+    public void the_user_enter_the_driver_information() {
+
+        String username=ConfigurationReader.get("driver_username");
+        String password=ConfigurationReader.get("driver_password");
+
+        LoginPage loginPage=new LoginPage();
+        loginPage.login(username,password);
+
+    }
+
+
+
+    @Then("the user should be able to login")
+    public void the_user_should_be_able_to_login() {
+        BrowserUtils.waitFor(3);
+        String actualTitle=Driver.get().getTitle();
+        Assert.assertEquals("Dashboard",actualTitle);
+
+
+    }
+
+}
