4c006b16c4f559073cfd41f6e4b0a97b14c989fb
h-yildiz <68542303+h-yildiz@users.noreply.github.com>
2021-10-27 23:41:26 +0200

    update plugin

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -4,15 +4,19 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+import static io.cucumber.junit.CucumberOptions.SnippetType.UNDERSCORE;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
-                "rerun:target/rerun.txt"},
+                "rerun:target/rerun.txt",
+        "pretty","summary" }, snippets = UNDERSCORE,
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@allUsers"
+        monochrome = true,
+        dryRun = true
+       // tags = "@wip"
 )
 
 public class CukesRunner {
