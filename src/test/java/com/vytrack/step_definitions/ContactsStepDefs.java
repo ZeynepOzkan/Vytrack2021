292c934b834f752d464ee07238507ff5fadeaf1a
h-yildiz <68542303+h-yildiz@users.noreply.github.com>
2021-11-03 16:22:15 +0100

    add step definition

---
+++ src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
@@ -0,0 +1,11 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.When;
+
+public class ContactsStepDefs {
+
+    @When("the user navigates to {string} {string}")
+    public void theUserNavigatesTo(String arg0, String arg1) {
+
+    }
+}
