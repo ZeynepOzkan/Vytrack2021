5a560b66293e35d371d2508b2c8baaf4b54aac77
h-yildiz <68542303+h-yildiz@users.noreply.github.com>
2021-11-03 16:21:31 +0100

    locators located

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -1,6 +1,22 @@
 package com.vytrack.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public abstract class BasePage {
+
+    @FindBy (css = "div>h1>a")
+    public WebElement HomePage;
+
+    @FindBy (css = "div#main-menu")
+    public List<WebElement> mainMenu;
+
+    @FindBy (css = "span.title.title-level-1")
+    public List<WebElement> menuOptions;
+
+
 
 
 
